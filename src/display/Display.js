9c300322312ccdeae8be4a52a9f2d58d97c6b9a0
eric <Eric355402@yahoo.com>
2019-12-06 10:48:18 -0700

    mvp complete

--- src/display/Display.js
+++ src/display/Display.js
@@ -8,13 +8,14 @@ const Display = ({ closed, locked }) => {
     <div className="display panel">
       <div className={lockedClass}>{locked ? 'Locked' : 'Unlocked'}</div>
       <div className={closedClass}>{closed ? 'Closed' : 'Open'}</div>
+      test
     </div>
   );
 };
 
 Display.defaultProps = {
   closed: false,
-  locked: false,
+  locked: false
 };
 
 export default Display;
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,19 @@
-// Test away!
+import React from 'react';
+import Display from './Display';
+import Controls from '../controls/Controls';
+import { render } from '@testing-library/react';
+import { get } from 'http';
+
+test('gate default values are correct', () => {
+  const { getByText } = render(<Display />);
+
+  getByText(/Unlocked/i);
+  getByText(/Open/i);
+});
+
+test('cannot be closed or opened if it is locked', () => {
+  const { getByText } = render(<Controls />);
+
+  const lockGateButton = getByText(/Lock Gate/i);
+  const closedGateButton = getByText(/Close Gate/i);
+});
